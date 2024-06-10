9ce8b9c79e986c872308e79fd29f9c70c1db54d0
ncfurtado <fur22004@byui.edu>
2024-06-10 15:04:21 -0600

    This is a simple change, using terminal as the method.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This language is compiled! It will take much longer to test code compared to Python.");
+        int x = 10;
+        Console.WriteLine("C# is a strongly typed language, you must give a type to each variable and it NEVER changes.");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(x+ i);
+        }
     }
 }
