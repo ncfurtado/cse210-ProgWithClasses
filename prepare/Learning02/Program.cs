85f9335fb776d2c8208dd773ba3d7f5d4ac702a1
ncfurtado <fur22004@byui.edu>
2024-06-17 18:21:30 -0600

    I finished the Learning02 Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft Corporation";
+        job1._jobTitle = "Intern";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
+
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Developer";
+        job2._startYear = 2016;
+        job2._endYear = 2017;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Daniel Game";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
+
     }
 }
